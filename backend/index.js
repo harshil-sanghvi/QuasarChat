fb766db18e7dd885b4e213f1b937776b37bc2a67
Harshil Sanghvi <67953038+harshil-sanghvi@users.noreply.github.com>
2024-01-06 22:14:06 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
-const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 
@@ -13,6 +14,8 @@ require("dotenv").config();
 //   methods: ["POST", "GET"],
 //   credentials: true,
 // }));
+
+const app = express();
 app.use(express.json());
 
 // Database connection setup
@@ -33,18 +36,24 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Server setup
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on port ${process.env.PORT}`)
-);
+// const server = app.listen(process.env.PORT, () =>
+//   console.log(`Server started on port ${process.env.PORT}`)
+// );
+
+const httpServer = createServer(app);
 
 // Socket.io setup
-const io = socket(server, {
+const io = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["POST", "GET"],
     credentials: true,
   },
 });
+
+httpServer.listen(process.env.PORT, () =>
+  console.log(`Server started on port ${process.env.PORT}`)
+);
 
 // Global map to store online users and their socket IDs
 global.onlineUsers = new Map();
